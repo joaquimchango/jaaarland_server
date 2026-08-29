a6febdd39393c7a601c462ad64a1b80b27813eee
ecommfox <medellint@gmail.com>
2026-08-29 10:49:13 +0100

    This time is the last

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+const cors = require("cors");
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
