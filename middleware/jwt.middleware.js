1f5d25d040325e5de2bbca746dab9cd27994419b
ecommfox <medellint@gmail.com>
2026-09-05 15:33:33 +0100

    modified : jwt middleware to add UserAuth in payload

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -7,7 +7,8 @@ const isAuthenticated = (req, res, next)=>{
 try{
  const token = req.headers.authorization.split(" ")[1];
  const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
- req.payload = decodedToken;
+ console.log(decodedToken);
+ req.payload.userAuth = decodedToken;
  next();
 }catch{
   res.status(401).json({
