c7c1d6f1f34a35d26138b5a803d8c89c0b02fef1
ecommfox <medellint@gmail.com>
2026-09-06 16:36:27 +0100

    Middleware jwt changed , added error handling and console log to re.payload(l11:26)

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -5,10 +5,20 @@ const jwt = require("jsonwebtoken");
 const isAuthenticated = (req, res, next)=>{
 
 try{
- const token = req.headers.authorization.split(" ")[1];
+
+ const authHeader = req.headers.authorization;
+
+ if (!authHeader) {
+      return res.status(401).json({
+        error: "Authorization header is missing",
+      });
+    }
+_
+ const token = authHeader.split(" ")[1];
  const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
- console.log(decodedToken);
- req.payload.userAuth = decodedToken;
+ 
+ req.payload = decodedToken;
+ console.log(req.payload)
  next();
 }catch{
   res.status(401).json({
